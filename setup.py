3f59140759292b76c696e870e798e82bcb4e9332
Sone-i <86225447+Sone-i@users.noreply.github.com>
2022-04-22 15:21:58 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,24 +1,31 @@
-import setuptools
+# -*- coding: utf-8
 
-with open("README.md", "r") as fh:
+import setuptools
+import csw
+
+with open('README.md', 'r') as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name="csw",
-	version="0.1.0",
-	author="Sone-i",
-	author_email="i18308@kagawa.kosen-ac.jp",
-	description="This is csw test.",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/Sone-i/csw",
-	packages=setuptools.find_packages(),
-	classifiers=[
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
+	name = 'csw',
+	version = csw.__version__,
+	author = 'Sone-i',
+	author_email = 'i18308@kagawa.kosen-ac.jp',
+	description = "文自在変換ツール",
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	url = 'https://github.com/Sone-i/csw',
+	download_url = 'https://github.com/Sone-i/csw',
+	packages = setuptools.find_packages(),
+	classifiers = [
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
 	],
 	entry_points = {
 		'console_scripts': ['csw = csw.csw:main']
 	},
-	python_requires='>=3.7',
+	python_requires = '>=3.7',
+	install_requires = [
+		'MeCab>=0.996'
+	]
 )
